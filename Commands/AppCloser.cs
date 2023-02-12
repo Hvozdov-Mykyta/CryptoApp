a9b768e9ce73f4621a89189e1cb4d67f67e1caf1
Mykyta Hvozdov <mykyta.hvozdov@gmail.com>
2023-02-12 19:16:05 +0200

    Created main window ViewModel, created button with binded command to close window, removed title bar

---
+++ Commands/AppCloser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CryptoApp.Commands
+{
+    internal class AppCloser : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            App.Current.MainWindow.Close();
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CryptoApp.Commands;
+using System.Windows.Input;
+
+namespace CryptoApp.ViewModels
+{
+    internal class MainWindowViewModel
+    {
+        private ICommand _appCloser;
+        public ICommand CloseApp
+        {
+            get
+            {
+                if (_appCloser == null)
+                    _appCloser = new AppCloser();
+                return _appCloser;
+            }
+            set
+            {
+                _appCloser = value;
+            }
+        }
+    }
+}
