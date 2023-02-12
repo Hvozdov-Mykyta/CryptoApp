0cbd6016627eb0c86f7c1424c1cddac61462238c
Mykyta Hvozdov <mykyta.hvozdov@gmail.com>
2023-02-12 17:42:32 +0200

    Added button 'Refresh' on home page to update coins data and ceated folder 'Commands' and class 'CoinsUpdater' with refresh logic

---
+++ Commands/CoinsUpdater.cs
@@ -0,0 +1,32 @@
+﻿using CryptoApp.ViewModels;
+using System;
+using System.Windows.Input;
+
+namespace CryptoApp.Commands
+{
+    internal class CoinsUpdater : ICommand
+    {
+        private HomePageViewModel homePageVM;
+
+        public CoinsUpdater(HomePageViewModel homePageVM)
+        {
+            this.homePageVM = homePageVM;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            homePageVM.UpdateCoinsList();
+        }
+    }
+}
--- Views/HomePage.xaml.cs
+++ Views/HomePage.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace CryptoApp
 {
-    /// <summary>
-    /// Interaction logic for HomePage.xaml
-    /// </summary>
     public partial class HomePage : Page
     {
         public HomePage()
